f804f0efda3bc8b53b4043cbde66743be03779ca
Akhmad Pebrian Yuniarso <46565766+Pebrian1999@users.noreply.github.com>
2021-06-22 08:40:07 +0800

    membuat subclass dan 3 atribut dan 2 method

---
+++ src/Biodata/subdata.java
@@ -0,0 +1,18 @@
+package Biodata;
+
+public class subdata extends data{
+    String namapenerima;
+    String jeniskelaminp;
+    String pekerjaanp;
+
+    public void namapenerima() {
+
+    }
+
+    public void tampilinfopenerima() {
+
+    }
+
+
+
+}
