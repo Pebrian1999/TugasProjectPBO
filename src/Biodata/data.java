04c9288d3fe4c7056842a211c237eeb67afc817e
Akhmad Pebrian Yuniarso <46565766+Pebrian1999@users.noreply.github.com>
2021-06-22 08:25:22 +0800

    membuat class dengan 8 attribut dan 4 method

--- src/Biodata/data.java
+++ src/Biodata/data.java
@@ -1,5 +1,28 @@
 package Biodata;
 
-public class data {
+    public class data {
+    public String nama;
+    public String jeniskelamin;
+    public String kota;
+    public String nohp;
+    public int tinggib;
+    public int beratb;
+    public String status;
+    public String pekerjaan;
 
+    public void tampildata() {
+
+    }
+
+    public void cetakdata(){
+
+    }
+
+    public void tampilnama(){
+
+    }
+
+    public void cetakkota(){
+
+    }
 }
